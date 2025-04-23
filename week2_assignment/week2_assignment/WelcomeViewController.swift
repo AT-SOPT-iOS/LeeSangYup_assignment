e72b9e2834ef7d1f5ef1c22621f2733092ebcc56
sangyup <ideallyup@naver.com>
2025-04-24 00:29:52 +0900

    :sparkles:: WelcomeViewController
    create WelcomeViewController

---
+++ week2_assignment/week2_assignment/WelcomeViewController.swift
@@ -0,0 +1,65 @@
+//
+//  WelcomeViewController.swift
+//  week2_assignment
+//
+//  Created by 이상엽 on 4/23/25.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    let logoImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "logo_tving")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    let welcomeLable: UILabel = {
+        let label = UILabel()
+        label.text = "님\n반가워요!"
+        label.font = .pretendardBold23
+        label.textColor = .white
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        return label
+    }()
+    let mainButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("메인으로", for: .normal)
+        button.backgroundColor = .redTving
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 3
+        return button
+    }()
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            self.view.backgroundColor = .black
+            setLayout()
+
+    }
+    
+    private func setLayout() {
+        self.view.addSubviews(logoImage,
+                              mainButton,
+                              welcomeLable)
+        
+        NSLayoutConstraint.activate([logoImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 58),
+                                     logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                                     logoImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                                     logoImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                                     logoImage.heightAnchor.constraint(equalTo: logoImage.widthAnchor, multiplier: 9.0/16.0)
+
+                                    ])
+        NSLayoutConstraint.activate([mainButton.topAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -96),
+                                     mainButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+                                     mainButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+                                     mainButton.heightAnchor.constraint(equalToConstant: 52)])
+        NSLayoutConstraint.activate([welcomeLable.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 67),
+                                     welcomeLable.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
+    }
+}
+
+#Preview{
+    WelcomeViewController()
+}
+
