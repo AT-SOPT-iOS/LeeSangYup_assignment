d1b47d89d49a67201af7dcd71a6653bad0a295da
sangyup <ideallyup@naver.com>
2025-04-23 16:25:46 +0900

    :lipstick:: Add Extension Font
    UIFont+

---
+++ week2_assignment/week2_assignment/Extension/UIFont+.swift
@@ -0,0 +1,31 @@
+//
+//  UIFont+.swift
+//  week2_assignment
+//
+//  Created by 이상엽 on 4/23/25.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static var pretendardRegular14: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 14)!
+    }
+
+    static var pretendardMedium23: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 23)!
+    }
+
+    static var pretendardSemiBold14: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 14)!
+    }
+
+    static var pretendardSemiBold15: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 15)!
+    }
+
+    static var pretendardBold23: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 23)!
+    }
+}
