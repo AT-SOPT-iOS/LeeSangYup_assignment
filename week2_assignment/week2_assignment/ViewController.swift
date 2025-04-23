0aa4993538b81dc3d687246b0da4688ac76aac58
sangyup <ideallyup@naver.com>
2025-04-23 15:46:50 +0900

    :tada:: Begin a project
    delete storyboard

---
+++ week2_assignment/week2_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2_assignment
+//
+//  Created by 이상엽 on 4/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
