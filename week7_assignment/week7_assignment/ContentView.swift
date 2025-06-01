3e2d6e08a92cbb1707cb04d8ffdc805406076788
sangyup <ideallyup@naver.com>
2025-06-01 14:53:37 +0900

    :tada: New Project
    SwiftUI TVING

---
+++ week7_assignment/week7_assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  week7_assignment
+//
+//  Created by 이상엽 on 6/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
